3d5f5a137ce2ede84e912ac51f833f7d738aba25
Leif Singer <git@singer.sh>
2016-11-23 12:41:46 +0100

    take tour steps from #7083

--- client/state/ui/guided-tours/contexts.js
+++ client/state/ui/guided-tours/contexts.js
@@ -2,6 +2,7 @@
 
 import config from 'config';
 import { getSectionName, isPreviewShowing, getSelectedSite } from 'state/ui/selectors';
+import { isFetchingNextPage, getQueryParams, getThemesList } from 'state/themes/themes-list/selectors';
 import { getCurrentUser } from 'state/current-user/selectors';
 
 export const inSection = sectionName => state =>
@@ -32,3 +33,14 @@ export const selectedSiteIsPreviewable = state =>
 
 export const selectedSiteIsCustomizable = state =>
 	getSelectedSite( state ) && getSelectedSite( state ).is_customizable;
+
+// Themes
+export const themeSearchResultsFound = state => {
+	const params = getQueryParams( state );
+	return params && params.search && params.search.length && ! isFetchingNextPage( state ) && getThemesList( state ).length > 0;
+};
+
+export const themeFilterChosen = filter => state => {
+	const params = getQueryParams( state );
+	return params && params.tier === filter;
+};
